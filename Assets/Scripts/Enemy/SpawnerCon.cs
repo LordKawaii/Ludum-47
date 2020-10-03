59813692d3fc7c6363d96e0f831053efb8a90fe8
LordKawaii <lordkawaii@gmail.com>
2020-10-03 11:47:28 -0700

    Added Spawners

---
+++ Assets/Scripts/Enemy/SpawnerCon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerCon : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject enemy;
+
+    public void Spawn(int num)
+    {
+        if (enemy != null)
+        {
+            for (int i = 0; i <= num; i++)
+            {
+                Instantiate(enemy, transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameCon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCon : MonoBehaviour
+{
+    [HideInInspector]
+    public static GameCon Instance;
+
+    public int loopTimes = 0;
+
+    [HideInInspector]
+    public List<GameObject> spawners;
+
+    
+    void Awake()
+    {        ///Set this to be main Game Controller
+        if (Instance)
+        {
+            Debug.Log("I already exist: destroying self");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        spawners = new List<GameObject>();
+
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Spawner"))
+        {
+            spawners.Add(obj);
+        }
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
