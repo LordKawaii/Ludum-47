20f7df506b61e0972075b22a2a67135588ddcf55
LordKawaii <lordkawaii@gmail.com>
2020-10-02 23:04:05 -0700

    Player Movement Added

---
+++ Assets/PlayerControlls.cs
@@ -0,0 +1,322 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerControlls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControlls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControlls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControlls"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerMovement"",
+            ""id"": ""aaec4dd5-cb82-45f8-867d-58cb1f1c5003"",
+            ""actions"": [
+                {
+                    ""name"": ""MoveRight"",
+                    ""type"": ""Button"",
+                    ""id"": ""ba814754-3853-407e-8866-bea7b227d615"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""MoveLeft"",
+                    ""type"": ""Button"",
+                    ""id"": ""423d6190-8781-4a2a-bc9d-a303ffcf8f83"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Duck"",
+                    ""type"": ""Button"",
+                    ""id"": ""3e65188e-3977-4cc5-a172-8d300b4f83d6"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""58e3aa74-c623-4182-b0c9-6c207485ae17"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""bcca7ffa-1967-48b7-aaca-a2e3316aae13"",
+                    ""path"": ""<Gamepad>/leftStick/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MoveRight"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""2c61e528-eca7-4915-b7ec-bf998d995ba2"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MoveRight"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""12be76fd-60fa-40c3-937a-1a8cd279d5b5"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MoveRight"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""12029681-cb71-4d01-991f-ab1786af843a"",
+                    ""path"": ""<Gamepad>/leftStick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MoveLeft"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""8a253cb7-90d6-4de4-bec9-2abe3819df4c"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MoveLeft"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""8647a272-fdeb-4743-8cb0-7a2d2a358221"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MoveLeft"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""53b8b555-2905-4ba0-b7c3-d752d0676686"",
+                    ""path"": ""<Gamepad>/rightStick/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Duck"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""98b1fd3e-ef01-4080-ae4f-fd091063d743"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Duck"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""022a8422-22c9-462d-9eb0-2ec799382b17"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Duck"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""79391aea-733f-4c0a-a554-2382204de169"",
+                    ""path"": ""<Gamepad>/leftStick/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""80c81ddf-f24d-4d60-8677-3ab458123395"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9b5ee6af-bdf7-4e8f-b04e-2575e5c38db7"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Player"",
+            ""bindingGroup"": ""Player"",
+            ""devices"": []
+        }
+    ]
+}");
+        // PlayerMovement
+        m_PlayerMovement = asset.FindActionMap("PlayerMovement", throwIfNotFound: true);
+        m_PlayerMovement_MoveRight = m_PlayerMovement.FindAction("MoveRight", throwIfNotFound: true);
+        m_PlayerMovement_MoveLeft = m_PlayerMovement.FindAction("MoveLeft", throwIfNotFound: true);
+        m_PlayerMovement_Duck = m_PlayerMovement.FindAction("Duck", throwIfNotFound: true);
+        m_PlayerMovement_Jump = m_PlayerMovement.FindAction("Jump", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerMovement
+    private readonly InputActionMap m_PlayerMovement;
+    private IPlayerMovementActions m_PlayerMovementActionsCallbackInterface;
+    private readonly InputAction m_PlayerMovement_MoveRight;
+    private readonly InputAction m_PlayerMovement_MoveLeft;
+    private readonly InputAction m_PlayerMovement_Duck;
+    private readonly InputAction m_PlayerMovement_Jump;
+    public struct PlayerMovementActions
+    {
+        private @PlayerControlls m_Wrapper;
+        public PlayerMovementActions(@PlayerControlls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @MoveRight => m_Wrapper.m_PlayerMovement_MoveRight;
+        public InputAction @MoveLeft => m_Wrapper.m_PlayerMovement_MoveLeft;
+        public InputAction @Duck => m_Wrapper.m_PlayerMovement_Duck;
+        public InputAction @Jump => m_Wrapper.m_PlayerMovement_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerMovement; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerMovementActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerMovementActions instance)
+        {
+            if (m_Wrapper.m_PlayerMovementActionsCallbackInterface != null)
+            {
+                @MoveRight.started -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnMoveRight;
+                @MoveRight.performed -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnMoveRight;
+                @MoveRight.canceled -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnMoveRight;
+                @MoveLeft.started -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnMoveLeft;
+                @MoveLeft.performed -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnMoveLeft;
+                @MoveLeft.canceled -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnMoveLeft;
+                @Duck.started -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnDuck;
+                @Duck.performed -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnDuck;
+                @Duck.canceled -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnDuck;
+                @Jump.started -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_PlayerMovementActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @MoveRight.started += instance.OnMoveRight;
+                @MoveRight.performed += instance.OnMoveRight;
+                @MoveRight.canceled += instance.OnMoveRight;
+                @MoveLeft.started += instance.OnMoveLeft;
+                @MoveLeft.performed += instance.OnMoveLeft;
+                @MoveLeft.canceled += instance.OnMoveLeft;
+                @Duck.started += instance.OnDuck;
+                @Duck.performed += instance.OnDuck;
+                @Duck.canceled += instance.OnDuck;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public PlayerMovementActions @PlayerMovement => new PlayerMovementActions(this);
+    private int m_PlayerSchemeIndex = -1;
+    public InputControlScheme PlayerScheme
+    {
+        get
+        {
+            if (m_PlayerSchemeIndex == -1) m_PlayerSchemeIndex = asset.FindControlSchemeIndex("Player");
+            return asset.controlSchemes[m_PlayerSchemeIndex];
+        }
+    }
+    public interface IPlayerMovementActions
+    {
+        void OnMoveRight(InputAction.CallbackContext context);
+        void OnMoveLeft(InputAction.CallbackContext context);
+        void OnDuck(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,12 +1,92 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour, PlayerControlls.IPlayerMovementActions
 {
+    [SerializeField]
+    float moveSpeed = 10f;
+    [SerializeField]
+    float maxSpeed = 20f;
+    [SerializeField]
+    float jumpForce = 100f;
+
+    bool canJump = true;
+
+    Vector2 moveForce;
+
+    Rigidbody2D rb2d;
+
+    public PlayerControlls controlls;
+    
+    private void Awake()
+    {
+        controlls = new PlayerControlls();
+        controlls.PlayerMovement.SetCallbacks(this);
+        moveForce = Vector2.zero;
+        if (rb2d == null)
+            rb2d = GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        rb2d.AddForce(moveForce * moveSpeed);
+        if (rb2d.velocity.magnitude > maxSpeed)
+            rb2d.velocity = rb2d.velocity.normalized * maxSpeed;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, .1f);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.tag == "Floor")
+            {
+                canJump = true;
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        controlls.PlayerMovement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controlls.PlayerMovement.Disable();
+    }
+
+    public void OnMoveRight(InputAction.CallbackContext context)
+    {
+        //Vector2 force = new Vector2(1, 0);
+        if (context.phase == InputActionPhase.Performed)
+            moveForce = new Vector2(1, 0);
+        else
+            moveForce = Vector2.zero;
+    }
+
+    public void OnMoveLeft(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+            moveForce = new Vector2(-1, 0);
+        else
+            moveForce = Vector2.zero;
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            if (canJump)
+            {
+                rb2d.AddForce(new Vector2(0, jumpForce));
+                canJump = false;
+            }
+        }
+
+    }
+
+    public void OnDuck(InputAction.CallbackContext context)
+    {
     }
 }
