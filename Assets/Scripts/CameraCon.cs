e7e14118068d88c1ecbde141927d40826a2c1e9c
LordKawaii <lordkawaii@gmail.com>
2020-10-02 23:29:39 -0700

    Implemented Camera Controlls

---
+++ Assets/Scripts/CameraCon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class CameraCon : MonoBehaviour
+{
+    Transform playerTrans;
+    float xLerp;
+    float yLerp;
+
+    [SerializeField]
+    float moveSpeed = 5f;
+
+    private void Awake()
+    {
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPos = Vector2.Lerp(transform.position, playerTrans.position, moveSpeed);
+        transform.position = new Vector3(newPos.x, newPos.y +1, -10);
+    }
+}
